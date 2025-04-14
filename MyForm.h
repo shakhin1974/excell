357a770493f2920541a1caedb000e6225fb68a75
shakhin1974 <57936556+shakhin1974@users.noreply.github.com>
2025-04-14 19:36:46 +0300

    MyForm.h

--- MyForm.h
+++ MyForm.h
@@ -69,22 +69,22 @@ namespace Project4 {
 			this->dataGridView1->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
 			this->dataGridView1->Location = System::Drawing::Point(12, 27);
 			this->dataGridView1->Name = L"dataGridView1";
-			this->dataGridView1->Size = System::Drawing::Size(509, 285);
+			this->dataGridView1->Size = System::Drawing::Size(544, 285);
 			this->dataGridView1->TabIndex = 0;
 			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(560, 113);
+			this->button1->Location = System::Drawing::Point(599, 113);
 			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(75, 23);
+			this->button1->Size = System::Drawing::Size(135, 23);
 			this->button1->TabIndex = 1;
-			this->button1->Text = L"button1";
+			this->button1->Text = L"Вывод с эксель";
 			this->button1->UseVisualStyleBackColor = true;
 			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
 			// 
 			// button2
 			// 
-			this->button2->Location = System::Drawing::Point(560, 157);
+			this->button2->Location = System::Drawing::Point(599, 157);
 			this->button2->Name = L"button2";
 			this->button2->Size = System::Drawing::Size(75, 23);
 			this->button2->TabIndex = 2;
@@ -94,15 +94,15 @@ namespace Project4 {
 			// label1
 			// 
 			this->label1->AutoSize = true;
-			this->label1->Location = System::Drawing::Point(560, 27);
+			this->label1->Location = System::Drawing::Point(599, 27);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(35, 13);
+			this->label1->Size = System::Drawing::Size(96, 13);
 			this->label1->TabIndex = 3;
-			this->label1->Text = L"label1";
+			this->label1->Text = L"Данные студента";
 			// 
 			// textBox1
 			// 
-			this->textBox1->Location = System::Drawing::Point(560, 70);
+			this->textBox1->Location = System::Drawing::Point(599, 70);
 			this->textBox1->Name = L"textBox1";
 			this->textBox1->Size = System::Drawing::Size(100, 20);
 			this->textBox1->TabIndex = 4;
@@ -136,26 +136,44 @@ namespace Project4 {
 		String^ connString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + filePath + ";Extended Properties='Excel 12.0 Xml;HDR=YES;'";
 		OleDbConnection^ conn = gcnew OleDbConnection(connString);
 		conn->Open();
-		OleDbCommand^ cmd = gcnew OleDbCommand("SELECT * FROM [list$A:E]", conn);
+		OleDbCommand^ cmd = gcnew OleDbCommand("SELECT * FROM [list$A:G]", conn);
 	///	OleDbDataAdapter^ adapter = gcnew OleDbDataAdapter(cmd);
 		OleDbDataReader^ reader = cmd->ExecuteReader();
 		DataTable^ dt = gcnew DataTable();
 	///	adapter->Fill(dt);
 	///	dataGridView1->DataSource = dt;
 		dataGridView1->Columns->Clear();
-		dataGridView1->ColumnCount = 2;
-		dataGridView1->Columns[0]->Name = "FIO";
-		dataGridView1->Columns[1]->Name = "Age";
+		dataGridView1->ColumnCount = 5;
+		dataGridView1->Columns[0]->Name = "Фамилия";
+		dataGridView1->Columns[1]->Name = "Имя";
+		dataGridView1->Columns[2]->Name = "Возраст";
+		dataGridView1->Columns[3]->Name = "Пол";
+		dataGridView1->Columns[4]->Name = "Группа";
+
+
+
 		if (reader->Read())
 	{
 	// Считывание данных из первой ячейки
-	String^ cellValue1 = reader[1]->ToString();
-	String^ cellValue2 = reader[2]->ToString();
+	String^ cellValue1 = reader[0]->ToString();
+	String^ cellValue2 = reader[1]->ToString();
+	String^ cellValue3 = reader[2]->ToString();
+	String^ cellValue4 = reader[3]->ToString();
+	String^ cellValue5 = reader[4]->ToString();
+
+
+
 			// Присваивание значения в textbox
 	dataGridView1->Rows[0]->Cells[0]->Value = cellValue1;
 	dataGridView1->Rows[0]->Cells[1]->Value = cellValue2;
+	dataGridView1->Rows[0]->Cells[2]->Value = cellValue3;
+	dataGridView1->Rows[0]->Cells[3]->Value = cellValue4;
+	dataGridView1->Rows[0]->Cells[4]->Value = cellValue5;
 
-	textBox1->Text = cellValue1; // Замените yourTextBox на имя вашего текстового поля
+
+
+
+	textBox1->Text = cellValue1 + " " + cellValue2;  
 	}
 	conn->Close();
 
